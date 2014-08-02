7cd230f0c49dce920cb14c99dba3551d60663f41
Chris DeLuca <bronzehedwick@gmail.com>
2014-08-01 20:20:10 -0400

    Remove footer attribution.

--- wp-content/themes/patriciahorvath/footer.php
+++ wp-content/themes/patriciahorvath/footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'patriciahorvath' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'patriciahorvath' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'patriciahorvath' ), 'patriciahorvath', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
